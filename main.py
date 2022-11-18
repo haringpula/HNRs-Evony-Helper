f85df057f51a18382d7c3b4b913a0a25b97f228b
haringpula <kingredpula31@gmail.com>
2022-11-18 23:20:58 +0800

    sync test name change

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ async def on_ready():
         print("Announcement Started")
 
 @bot.command()
-async def sync(ctx):
+async def syncing(ctx):
     guild = ctx.guild or discord.Object(id=835899395219652671)  
     # you can use a full discord.Guild as the method accepts a Snowflake
     bot.tree.copy_global_to(guild=guild)
