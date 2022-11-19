b179cb9a0bb38856a6a01cad1450c8264e9f1221
haringpula <kingredpula31@gmail.com>
2022-11-19 11:29:49 +0800

    activity test

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ intents = discord.Intents.all()
 intents.message_content = True
 bot = commands.Bot(command_prefix=prefix,  intents=intents)
 activity = discord.Activity(
-    name=f"{len(bot.guilds)} servers watched",
+    name=f"{len(bot.fetch_guilds())} | /help",
     type=discord.ActivityType.watching)
 bot.remove_command('help')
 
